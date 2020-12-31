319200b43564d785491e6b3bfd6fe254d39f01fc
Nikita Mazalov <russi4wtf@gmail.com>
2020-12-31 13:55:54 +0700

    about to deploy

--- my_site/settings.py
+++ my_site/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "os.environ.get('SECRET_KEY')"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,13 +76,10 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    
 
 
 # Password validation
